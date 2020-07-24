0d2ef826bc36e0eab1a66fef8eb7e63d657ad9a7
ginami0129g <ginami0129n@naver.com>
2020-07-24 21:27:07 +0900

    Solved Lv_2 예상 대진표
    규칙을 찾아 푼 문제 재미있었다.

---
+++ src/programmers/Lv_2 예상 대진표.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    while(a != b) {
+        ++answer;
+        a = ++a / 2;
+        b = ++b / 2;
+    }
+    return answer;
+}
