ea58721d5f1ab7b2b7d45d72facb6a715508da5d
ginami0129g <ginami0129n@naver.com>
2020-11-10 23:41:58 +0900

    Create BOJ_8393.cpp

--- src/noj_am/BOJ_8393.cpp
+++ src/noj_am/BOJ_8393.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#define MAX_SIZE 10001
+
 using namespace std;
 
-int main(void) {
-  int n, result = 0;
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cin >> n;
-  while (n) {
-    result += n;
-    --n;
+int main(void)
+{
+  int N, sum = 0;
+  cin >> N;
+  while (N)
+  {
+    sum += N;
+    --N;
   }
-  cout << result << '\n';
+  cout << sum << '\n';
 }
