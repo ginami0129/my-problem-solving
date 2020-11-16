8529b4c8c6f21aa3b1893d63deec603ed4c6f926
ginami0129g <ginami0129n@naver.com>
2020-11-16 14:23:03 +0900

    Resolve #20

--- src/noj_am/BOJ_2156.cpp
+++ src/noj_am/BOJ_2156.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-#include <algorithm>
-#define MAX_SIZE 10001
 using namespace std;
 
-int main(void) {
-  int n;
-  int a[MAX_SIZE] = {0, }; 
-  int d[MAX_SIZE] = {0, };
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cin >> n;
-  for (int i = 1; i <= n; ++i) {
+int main(void)
+{
+  int N;
+  int d[10001] = {0};
+  int a[10001] = {0};
+  cin >> N;
+  for (int i = 1; i <= N; ++i)
+  {
     cin >> a[i];
   }
   d[1] = a[1];
-  d[2] = a[1] + a[2];
-  for (int i = 3; i <= n; ++i) {
-    d[i] = max(d[i-2]+a[i], d[i-3]+a[i-1]+a[i]);
-    d[i] = max(d[i], d[i-1]);
+  d[2] = d[1] + a[2];
+  for (int i = 3; i <= N; ++i)
+  {
+    d[i] = max(d[i - 3] + a[i - 1] + a[i], d[i - 2] + a[i]);
+    d[i] = max(d[i], d[i - 1]);
   }
-  cout << d[n] << '\n';
+  cout << d[N] << '\n';
 }
