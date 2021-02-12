aad628e8b8c41bcd221d97314af15760283ed413
ginami0129g <ginami0129n@naver.com>
2021-02-12 23:23:14 +0900

    Update BOJ_11055.cpp

--- src/noj_am/BOJ_11055.cpp
+++ src/noj_am/BOJ_11055.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-#include <algorithm>
-#define MAX_SIZE 1001
+#define MAX 1001
 using namespace std;
 
 int main(void) {
-  int N, temp, result = 0;
-  int A[MAX_SIZE] = {0, };
-  int d[MAX_SIZE] = {0, };
+  int N, result = 0;
+  int A[MAX] = {0};
+  int d[MAX] = {0};
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cin >> N;
-  for (int i = 1; i <= N; ++i) {
+  for (int i = 0; i < N; ++i) {
     cin >> A[i];
-    temp = 0;
     for (int j = 0; j < i; ++j) {
-      if (A[j] < A[i])
-        temp = max(temp, d[j]);
+      if (A[j] < A[i]) {
+        d[i] = max(d[i], d[j]);
+      }
     }
-    d[i] = A[i] + temp;
+    d[i] += A[i];
     result = max(result, d[i]);
   }
   cout << result << '\n';
