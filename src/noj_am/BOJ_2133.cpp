4eae8437200d2507b57fa04a0c405ea825ee9935
ginami0129g <ginami0129n@naver.com>
2021-02-14 19:03:02 +0900

    Update BOJ_2133.cpp

--- src/noj_am/BOJ_2133.cpp
+++ src/noj_am/BOJ_2133.cpp
@@ -1,17 +1,18 @@
+#include <cmath>
 #include <iostream>
-#define MAX_SIZE 31
+#define MAX 31
 using namespace std;
 
 int main(void) {
   int N;
-  int d[MAX_SIZE] ={1, 0, 3, };
+  int d[MAX] = {1};
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cin >> N;
-  for (int i = 4; i <= N; i+=2) {
-    d[i] =  3*d[i-2];
-    for (int j = i-4; j >= 0; j-=2) {
-      d[i] += 2*d[j];
+  for (int i = 2; i <= N; i += 2) {
+    d[i] = d[i - 2] * 3;
+    for (int j = 0; j <= i - 4; j += 2) {
+      d[i] += d[j] * 2;
     }
   }
   cout << d[N] << '\n';
