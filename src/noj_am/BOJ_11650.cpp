ff154cc16a9082ab90d792d0d5b6f0dcc37472c5
ginami0129g <ginami0129n@naver.com>
2021-02-20 02:07:28 +0900

    Update BOJ_11650.cpp

--- src/noj_am/BOJ_11650.cpp
+++ src/noj_am/BOJ_11650.cpp
@@ -4,25 +4,17 @@
 #define MAX 100000
 using namespace std;
 
-bool cmp(pair<int, int> P1, pair<int, int> P2) {
-  if (P1.first == P2.first) {
-    return P1.second < P2.second;
-  } else {
-    return P1.first < P2.first;
-  }
-}
-
 int main(void) {
   int N;
-  pair<int, int> P[MAX];
-  cin.tie(NULL);
+  pair<int, int> p[MAX];
   ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
   cin >> N;
   for (int i = 0; i < N; ++i) {
-    cin >> P[i].first >> P[i].second;
+    cin >> p[i].first >> p[i].second;
   }
-  sort(P, P + N, cmp);
+  sort(p, p + N);
   for (int i = 0; i < N; ++i) {
-    cout << P[i].first << " " << P[i].second << '\n';
+    cout << p[i].first << " " << p[i].second << '\n';
   }
 }
