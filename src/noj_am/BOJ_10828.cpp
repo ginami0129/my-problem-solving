562776e668119790755b1352146c610c02421391
ginami0129g <ginami0129n@naver.com>
2021-02-22 01:54:03 +0900

    Update BOJ_10828.cpp

--- src/noj_am/BOJ_10828.cpp
+++ src/noj_am/BOJ_10828.cpp
@@ -1,38 +1,28 @@
-#include <stack>
 #include <iostream>
-#include <string>
+#define MAX 10000
 using namespace std;
 
-int main() {
-  int N;
-  stack<int> s;
-  string command;
+int main(void) {
+  int N, data;
+  string str;
+  int top = -1;
+  int stack[MAX] = {0};
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cin >> N;
-  cin.get();
-  while (N) {
-    getline(cin, command);
-    if (command.find("push") == 0) {
-      s.push(stoi(command.substr(5)));
-    } else if (command.find("pop") == 0) {
-      if (s.empty()) {
-        cout << -1 << '\n';
-      } else {
-        cout << s.top() << '\n';
-        s.pop();
-      }
-    } else if (command.find("size") == 0) {
-      cout << s.size() << '\n';
-    } else if (command.find("empty") == 0) {
-      cout << s.empty() << '\n';
-    } else if (command.find("top") == 0) {
-      if (s.empty()) {
-        cout << -1 << '\n';
-      } else {
-        cout << s.top() << '\n';
-      }
+  while (N--) {
+    cin >> str;
+    if (str == "push") {
+      cin >> data;
+      stack[++top] = data;
+    } else if (str == "pop") {
+      cout << (top == -1 ? top : stack[top--]) << '\n';
+    } else if (str == "top") {
+      cout << (top == -1 ? top : stack[top]) << '\n';
+    } else if (str == "size") {
+      cout << top + 1 << '\n';
+    } else if (str == "empty") {
+      cout << (top == -1 ? 1 : 0) << '\n';
     }
-    --N;
   }
 }
