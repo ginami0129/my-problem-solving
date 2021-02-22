af522300ed5a1cae5640719b05507d3a2a2b16b2
ginami0129g <ginami0129n@naver.com>
2021-02-23 02:34:11 +0900

    Update BOJ_9012.cpp

--- src/noj_am/BOJ_9012.cpp
+++ src/noj_am/BOJ_9012.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
-#include <stack>
 #include <string>
 using namespace std;
 
-string IsCorrect(const string &input) {
-  stack<char> check;
-  for (int i = 0; i < input.size(); ++i) {
-      if (input[i] == '(' || input[i] == '[') {
-        check.push(input[i]);
-      } else if (check.empty()) {
-        return "NO";
-      } else if(input[i] == ')' && check.top() == '(') {
-        check.pop();
-      } else if (input[i] == ']' && check.top() == '[') {
-        check.pop();
-      }
-    }
-    return check.empty() ? "YES" : "NO";
-}
-
 int main(void) {
-  int T;
+  int N, top = 0;
   string input;
-  stack<char> check;
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  cin >> T; 
-  cin.get();
-  while (T) {
+  cin >> N;
+  while (N--) {
     cin >> input;
-    cin.get();
-    cout << IsCorrect(input) << '\n';
-    --T;
+    for (int i = 0; i < input.size(); ++i) {
+      if (input[i] == '(') {
+        ++top;
+      } else {
+        --top;
+        if (top < 0) break;
+      }
+    }
+    cout << (top == 0 ? "YES" : "NO") << '\n';
+    top = 0;
   }
 }
