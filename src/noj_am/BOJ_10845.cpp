a048cf123357c3fc576c571b40a61d73c9d831d5
ginami0129g <ginami0129n@naver.com>
2021-02-24 16:42:51 +0900

    Update BOJ_10845.cpp

--- src/noj_am/BOJ_10845.cpp
+++ src/noj_am/BOJ_10845.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
 #include <string>
-#include <queue>
+#define MAX 100000
 using namespace std;
 
 int main(void) {
-  queue<int> q;
-  string command;
   int N;
+  int queue[MAX] = {0};
+  int front = 0, rear = -1;
+  int count;
+  string input;
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cin >> N;
-  // 공백 처리
-  cin.get();
-  while (N) {
-    // 명령어 입력받기
-    getline(cin, command);
-    // 명령어에 따라 큐명령어 실행
-    if (command.find("push") == 0) {
-      // 명령어에서 숫자 추출
-      q.push(stoi(command.substr(5)));
-    } else if (command.find("pop") == 0) {
-      if (q.empty()) {
-        cout << -1 << '\n';
-      } else {
-        cout << q.front() << '\n';
-        q.pop();
-      }
-    } else if (command.find("size") == 0) {
-      cout << q.size() << '\n';
-    } else if (command.find("empty") == 0) {
-      cout << q.empty() << '\n';
-    } else if (command.find("front") == 0) {
-      cout << (q.empty() ? -1 : q.front()) << '\n';
-    } else if (command.find("back") == 0) {
-      cout << (q.empty() ? -1 : q.back()) << '\n';
+  while (N--) {
+    cin >> input;
+    if (input == "push") {
+      cin >> queue[++rear];
+    } else if (input == "pop") {
+      cout << (front > rear ? -1 : queue[front++]) << '\n';
+    } else if (input == "size") {
+      cout << max(0, rear - front + 1) << '\n';
+    } else if (input == "empty") {
+      cout << (front > rear) << '\n';
+    } else if (input == "front") {
+      cout << (front > rear ? -1 : queue[front]) << '\n';
+    } else if (input == "back") {
+      cout << (front > rear ? -1 : queue[rear]) << '\n';
     }
-    --N;
   }
 }
