87026153e8205b866d42f55e16ef245d2ef7fca9
ginami0129g <ginami0129n@naver.com>
2021-02-26 15:52:26 +0900

    Update BOJ_10866.cpp

--- src/noj_am/BOJ_10866.cpp
+++ src/noj_am/BOJ_10866.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main(void) {
-  int N;
+  int N, count = 0;
+  int front = 0, rear = MAX - 1;
   int deque[MAX] = {0};
-  int count = 0, front = MAX - 1, rear = 0;
   string input;
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
@@ -14,37 +14,31 @@ int main(void) {
   while (N--) {
     cin >> input;
     if (input == "push_front") {
-      front = (front + 1) % MAX;
+      front = (front - 1 + MAX) % MAX;
       cin >> deque[front];
       ++count;
     } else if (input == "push_back") {
-      rear = (rear + MAX - 1) % MAX;
+      rear = (rear + 1) % MAX;
       cin >> deque[rear];
       ++count;
-    } else if (input == "pop_back") {
-      if (count == 0) {
-        cout << -1 << '\n';
-      } else {
-        cout << deque[rear] << '\n';
-        rear = (rear + 1) % MAX;
-        --count;
-      }
-    } else if (input == "pop_front") {
-      if (count == 0) {
-        cout << -1 << '\n';
-      } else {
-        cout << deque[front] << '\n';
-        front = (front - 1 + MAX) % MAX;
-        --count;
-      }
     } else if (input == "size") {
       cout << count << '\n';
     } else if (input == "empty") {
       cout << (count == 0) << '\n';
+    } else if (count == 0) {
+      cout << -1 << '\n';
+    } else if (input == "pop_front") {
+      cout << deque[front] << '\n';
+      front = (front + 1) % MAX;
+      --count;
+    } else if (input == "pop_back") {
+      cout << deque[rear] << '\n';
+      rear = (rear - 1 + MAX) % MAX;
+      --count;
     } else if (input == "front") {
-      cout << (count == 0 ? -1 : deque[front]) << '\n';
+      cout << deque[front] << '\n';
     } else if (input == "back") {
-      cout << (count == 0 ? -1 : deque[rear]) << '\n';
+      cout << deque[rear] << '\n';
     }
   }
 }
