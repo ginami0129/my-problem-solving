d79407a3804eeff91c4750d01022e129931bde80
ginami0129g <ginami0129n@naver.com>
2021-02-26 16:06:13 +0900

    Update BOJ_10824.cpp

--- src/noj_am/BOJ_10824.cpp
+++ src/noj_am/BOJ_10824.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(void) {
-  string A, B, C, D;
+  long long A, B, C, D;
+  int t1 = 1, t2 = 1;
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cin >> A >> B >> C >> D;
-  cout << stoll(A + B) + stoll(C + D) << '\n';
+  while (B / t1 > 0) t1 *= 10;
+  while (D / t2 > 0) t2 *= 10;
+  A = A * t1 + B;
+  C = C * t2 + D;
+  cout << A + C << '\n';
 }
