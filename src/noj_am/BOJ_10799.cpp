543f0c47837641df3bc51f894a61cf893f64d409
ginami0129g <ginami0129n@naver.com>
2021-02-26 15:23:39 +0900

    Update BOJ_10799.cpp

--- src/noj_am/BOJ_10799.cpp
+++ src/noj_am/BOJ_10799.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <string>
+#define MAX 100001
 using namespace std;
 
 int main(void) {
-  string input;
-  int top = 1;
-  int result = 0;
-  cin >> input;
+  int top = 0, result = 0;
+  char input[MAX] = {0};
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  for (int i = 1; i < input.size(); ++i) {
+  cin >> input;
+  for (int i = 0; input[i]; ++i) {
     if (input[i] == '(') {
       ++top;
     } else {
       --top;
-      result += (input[i - 1] == ')' ? 1 : top);
+      result += input[i - 1] == ')' ? 1 : top;
     }
   }
   cout << result << '\n';
