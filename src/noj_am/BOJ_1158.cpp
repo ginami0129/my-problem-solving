9ce77b1d0187a2dd2d5608f8844a00cd4b8ab3ad
ginami0129g <ginami0129n@naver.com>
2021-02-26 20:51:37 +0900

    Update BOJ_1158.cpp

--- src/noj_am/BOJ_1158.cpp
+++ src/noj_am/BOJ_1158.cpp
@@ -1,44 +1,37 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-typedef struct NodeRecord {
+typedef struct Node {
   int data;
-  struct NodeRecord *next;
+  Node *next;
 } Node;
 
 int main(void) {
-  int N, K;
-  string result = "<";
-  // 더미노드 생성
-  Node *head = new Node;
+  int N, K, count;
+  Node *head = new Node();
   Node *temp = head;
   Node *node = nullptr;
-  head->data = 1;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
   cin >> N >> K;
-  // 원형 연결리스트 생성
-  for(int i = 1; i <= N; ++i) {
-    node = new Node;
+  for (int i = 1; i <= N; ++i) {
+    node = new Node();
     node->data = i;
     temp->next = node;
     temp = temp->next;
   }
-  // 마지막노드는 처음노드(더미노드 다음)
   temp->next = head->next;
-  // 순회하면서 이전노드 찾아 링크 정리후 다음노드 삭제 
-  temp = head;
-  for(int i = 0; N > 0; ++i){
-    if (i % K == K-1) {
-      node = temp->next;
-      result += to_string(node->data) + ", ";
-      temp->next = node->next;
-      delete(node);
-      N--;
-    } else {
+  cout << '<';
+  while (N > 1) {
+    count = K;
+    while (--count) {
       temp = temp->next;
     }
+    node = temp->next;
+    temp->next = node->next;
+    cout << node->data << ", ";
+    --N;
+    delete (node);
   }
-  // 결과 출력
-  result[result.size() - 2] = '>';
-  cout << result << endl;
+  cout << temp->data << ">" << '\n';
 }
