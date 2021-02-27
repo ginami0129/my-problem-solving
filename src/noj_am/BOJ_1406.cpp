613bbda7e074893079c10cccdbba828135915cf1
ginami0129g <ginami0129n@naver.com>
2021-02-27 15:07:26 +0900

    Update BOJ_1406.cpp

--- src/noj_am/BOJ_1406.cpp
+++ src/noj_am/BOJ_1406.cpp
@@ -9,59 +9,56 @@ typedef struct Node {
 } Node;
 
 int main(void) {
-  char input[MAX] = {0};
-  char command;
   int M;
-  Node* cursor = nullptr;
+  char str[MAX] = {0};
+  char command;
   Node* node = nullptr;
-  Node* head = new Node();
-  Node* tail = new Node();
+  Node* cursor = nullptr;
+  Node* head = new Node;
+  Node* tail = new Node;
   head->next = tail;
   head->prev = nullptr;
+  tail->next = nullptr;
   tail->prev = head;
-  tail->prev = nullptr;
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  cin >> input;
+  cin >> str;
   cursor = head;
-  for (int i = 0; input[i]; ++i, cursor = cursor->next) {
-    node = new Node();
-    node->data = input[i];
+  for (int i = 0; str[i]; ++i) {
+    node = new Node;
+    node->data = str[i];
     node->prev = cursor;
     node->next = cursor->next;
     cursor->next->prev = node;
     cursor->next = node;
+    cursor = cursor->next;
   }
-  cursor = tail;
   cin >> M;
+  cursor = tail;
   while (M--) {
     cin >> command;
-    if (command == 'L') {
-      if (cursor->prev->prev != nullptr) {
-        cursor = cursor->prev;
-      }
-    } else if (command == 'D') {
-      if (cursor->next != nullptr) {
-        cursor = cursor->next;
-      }
-    } else if (command == 'B') {
-      if (cursor->prev->prev != nullptr) {
-        node = cursor->prev;
-        cursor->prev = node->prev;
-        node->prev->next = cursor;
-        delete (node);
-      }
+    if (command == 'L' && cursor->prev->prev) {
+      cursor = cursor->prev;
+    } else if (command == 'D' && cursor->next) {
+      cursor = cursor->next;
+    } else if (command == 'B' && cursor->prev->prev) {
+      node = cursor->prev;
+      cursor->prev = node->prev;
+      node->prev->next = cursor;
+      delete (node);
     } else if (command == 'P') {
-      node = new Node();
+      node = new Node;
       cin >> node->data;
-      node->next = cursor;
       node->prev = cursor->prev;
+      node->next = cursor;
       cursor->prev->next = node;
       cursor->prev = node;
     }
   }
-  for (cursor = head->next; cursor->next; cursor = cursor->next) {
+  cursor = head->next;
+  while (cursor->next) {
     cout << cursor->data;
+    cursor = cursor->next;
   }
   cout << '\n';
 }
