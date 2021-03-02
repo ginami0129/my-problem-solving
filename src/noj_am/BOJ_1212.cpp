cfa75949f732f0b8d70e0f8f175627742bfc5833
ginami0129g <ginami0129n@naver.com>
2021-03-02 23:25:52 +0900

    Update BOJ_1212.cpp

--- src/noj_am/BOJ_1212.cpp
+++ src/noj_am/BOJ_1212.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main(void) {
-  string input, result;
+  string num, result;
   string d[8] = {"000", "001", "010", "011", "100", "101", "110", "111"};
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  cin >> input;
-  if (input == "0") {
+  cin >> num;
+  for (int i = 0; i < num.size(); ++i) {
+    result.append(d[num[i] - '0']);
+  }
+  if (num == "0") {
     cout << 0 << '\n';
   } else {
-    for (int i = 0; i < input.size(); ++i) {
-      result.append(d[input[i] - '0']);
-    }
-    int index = result.find('1');
-    cout << result.substr(index) << '\n';
+    int i = result.find('1');
+    cout << result.substr(i) << '\n';
   }
 }
