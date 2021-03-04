05792ea969f2c3d4dcec5661c8967724893cd3a6
ginami0129g <ginami0129n@naver.com>
2021-03-04 20:24:41 +0900

    Update BOJ_1373.cpp

--- src/noj_am/BOJ_1373.cpp
+++ src/noj_am/BOJ_1373.cpp
@@ -1,18 +1,25 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
 int main(void) {
-  string result, temp = "";
-  ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  cin >> temp;
-  while ((result.size() + temp.size()) % 3) {
-    result.push_back('0');
+  ios_base::sync_with_stdio(false);
+
+  string num;
+  cin >> num;
+  reverse(num.begin(), num.end());
+  while (num.size() % 3) {
+    num.push_back('0');
   }
-  temp = result + temp;
-  result.clear();
-  for (int i = 0; i < temp.size(); i += 3) {
-    result.push_back('0' + (temp[i] - '0') * 4 + (temp[i + 1] - '0') * 2 + temp[i + 2] - '0');
+  reverse(num.begin(), num.end());
+  string result;
+  for (int i = 0; i < num.size(); i += 3) {
+    char c = '0';
+    c += (num[i] - '0') * 4;
+    c += (num[i + 1] - '0') * 2;
+    c += (num[i + 2] - '0') * 1;
+    result.push_back(c);
   }
   cout << result << '\n';
 }
