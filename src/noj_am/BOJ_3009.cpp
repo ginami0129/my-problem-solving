9280884915c9c932803dc478086592c0b7c11756
ginami0129g <ginami0129n@naver.com>
2021-03-06 00:25:36 +0900

    Update BOJ_3009.cpp

--- src/noj_am/BOJ_3009.cpp
+++ src/noj_am/BOJ_3009.cpp
@@ -6,24 +6,11 @@ int main(void) {
   cin.tie(NULL);
   ios_base::sync_with_stdio(false);
 
-  pair<int, int> p1, p2, p3;
-  cin >> p1.first >> p1.second;
-  cin >> p2.first >> p2.second;
-  cin >> p3.first >> p3.second;
-
-  if (p1.first == p2.first) {
-    cout << p3.first << ' ';
-  } else if (p2.first == p3.first) {
-    cout << p1.first << ' ';
-  } else {
-    cout << p2.first << ' ';
+  pair<int, int> input;
+  pair<int, int> result;
+  while (cin >> input.first >> input.second) {
+    result.first ^= input.first;
+    result.second ^= input.second;
   }
-
-  if (p1.second == p2.second) {
-    cout << p3.second << ' ';
-  } else if (p2.second == p3.second) {
-    cout << p1.second << ' ';
-  } else {
-    cout << p2.second << ' ';
-  }
+  cout << result.first << ' ' << result.second << '\n';
 }
