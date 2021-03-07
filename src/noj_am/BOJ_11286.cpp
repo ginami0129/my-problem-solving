23bb30e5bfb173467be48867bf00f3bbfc323fda
ginami0129g <ginami0129n@naver.com>
2021-03-07 21:59:16 +0900

    Update BOJ_11286.cpp

--- src/noj_am/BOJ_11286.cpp
+++ src/noj_am/BOJ_11286.cpp
@@ -1,13 +1,19 @@
 #include <cmath>
 #include <iostream>
-#define MAX 100010
+#include <queue>
+#include <vector>
+
 using namespace std;
 
-void swap(int &a, int &b) {
-  int temp = a;
-  a = b;
-  b = temp;
-}
+struct cmp {
+  bool operator()(int a, int b) {
+    if (abs(a) == abs(b)) {
+      return a > b;
+    } else {
+      return abs(a) > abs(b);
+    }
+  }
+};
 
 int main(void) {
   cin.tie(NULL);
@@ -15,50 +21,17 @@ int main(void) {
 
   int N;
   cin >> N;
-  long long d[MAX] = {0};
-  int count = 0;
+  priority_queue<int, vector<int>, cmp> q;
   while (N--) {
-    int num;
-    cin >> num;
-    if (num > 0 || num < 0) {
-      d[count] = num;
-      int child = count;
-      int parent = (child - 1) / 2;
-      while (child > 0 && abs(d[parent]) >= abs(d[child])) {
-        swap(d[parent], d[child]);
-        if (abs(d[parent]) == abs(d[child]) && d[parent] > d[child]) {
-          swap(d[parent], d[child]);
-        }
-        child = parent;
-        parent = (child - 1) / 2;
-      }
-      ++count;
-    } else if (count == 0) {
+    int x;
+    cin >> x;
+    if (abs(x) > 0) {
+      q.push(x);
+    } else if (q.size() == 0) {
       cout << 0 << '\n';
     } else {
-      cout << d[0] << '\n';
-      d[0] = d[count - 1];
-      count = count - 1;
-      int parent = 0;
-      while (parent < count && 2 * parent + 1 < count) {
-        int child = 2 * parent + 1;
-        if (child + 1 < count && abs(d[child]) >= abs(d[child + 1])) {
-          if (abs(d[child]) == abs(d[child + 1]) && d[child] < d[child + 1]) {
-            child = child;
-          } else {
-            child = child + 1;
-          }
-        }
-        if (abs(d[child]) <= abs(d[parent])) {
-          if (abs(d[child]) == abs(d[parent]) && d[child] > d[parent]) {
-            parent = child;
-          } else {
-            swap(d[child], d[parent]);
-            parent = child;
-          }
-        } else
-          break;
-      }
+      cout << q.top() << '\n';
+      q.pop();
     }
   }
 }
