f13d96b07f5d324184bdb6f40f10ccc5547aafb3
ginami0129g <ginami0129n@naver.com>
2021-03-11 00:10:20 +0900

    Update BOJ_11286.cpp

--- src/noj_am/BOJ_11286.cpp
+++ src/noj_am/BOJ_11286.cpp
@@ -2,36 +2,34 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-
 using namespace std;
 
 struct cmp {
-  bool operator()(int a, int b) {
-    if (abs(a) == abs(b)) {
-      return a > b;
+  bool operator()(int x, int y) {
+    if (abs(x) == abs(y)) {
+      return x > y;
     } else {
-      return abs(a) > abs(b);
+      return abs(x) > abs(y);
     }
   }
 };
+priority_queue<int, vector<int>, cmp> pq;
+int N, x;
 
 int main(void) {
   cin.tie(NULL);
   ios_base::sync_with_stdio(false);
 
-  int N;
   cin >> N;
-  priority_queue<int, vector<int>, cmp> q;
   while (N--) {
-    int x;
     cin >> x;
-    if (abs(x) > 0) {
-      q.push(x);
-    } else if (q.size() == 0) {
+    if (x != 0) {
+      pq.push(x);
+    } else if (pq.size() == 0) {
       cout << 0 << '\n';
     } else {
-      cout << q.top() << '\n';
-      q.pop();
+      cout << pq.top() << '\n';
+      pq.pop();
     }
   }
 }
