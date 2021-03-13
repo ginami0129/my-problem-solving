7ac2e5ffb7d7842831615d17b3f90aad35ddd88d
ginami0129g <ginami0129n@naver.com>
2021-03-14 01:56:25 +0900

    Update BOJ_2178.cpp

--- src/noj_am/BOJ_2178.cpp
+++ src/noj_am/BOJ_2178.cpp
@@ -2,9 +2,9 @@
 #include <queue>
 using namespace std;
 
-int M, N;
-int v[101][101], d[] = {0, 0, -1, 1};
 queue<pair<int, int> > q;
+int v[101][101], d[] = {0, 0, -1, 1};
+int N, M;
 
 int main(void) {
   cin >> N >> M;
@@ -13,19 +13,19 @@ int main(void) {
       scanf("%1d", &v[i][j]);
     }
   }
-
   q.push(make_pair(0, 0));
+  v[0][0] = 0;
   while (!q.empty()) {
     int x = q.front().first, y = q.front().second;
-    q.pop();
     for (int i = 0; i < 4; ++i) {
       int nx = x + d[i], ny = y + d[3 - i];
-      if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
       if (v[nx][ny] == 1) {
         q.push(make_pair(nx, ny));
         v[nx][ny] = v[x][y] + 1;
       }
     }
+    q.pop();
   }
-  cout << v[N - 1][M - 1] << '\n';
+  cout << v[N - 1][M - 1] + 1 << '\n';
 }
