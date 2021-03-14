ae7721c586dbf090f41dd2a2945dc5e805a0f27b
ginami0129g <ginami0129n@naver.com>
2021-03-15 02:44:46 +0900

    Update BOJ_1697.cpp

--- src/noj_am/BOJ_1697.cpp
+++ src/noj_am/BOJ_1697.cpp
@@ -2,41 +2,35 @@
 #include <queue>
 using namespace std;
 
+int check[100001];
 queue<int> q;
-bool flag[100001];
-int N, M;
+int N, K;
 
 int main(void) {
   cin.tie(NULL);
   ios_base::sync_with_stdio(false);
 
-  cin >> N >> M;
+  cin >> N >> K;
   q.push(N);
-  flag[N] = true;
-  int r = 0;
+  check[N] = true;
   while (!q.empty()) {
-    int size = q.size();
-    while (size--) {
-      int cur = q.front();
-      if (cur == M) {
-        cout << r << "\n";
-        return 0;
-      }
-      if (cur - 1 >= 0 && !flag[cur - 1]) {
-        q.push(cur - 1);
-        flag[cur - 1] = true;
-      }
-      if (cur + 1 < 100001 && !flag[cur + 1]) {
-        q.push(cur + 1);
-        flag[cur + 1] = true;
-      }
-      if (cur * 2 < 100001 && !flag[cur * 2]) {
-        q.push(cur * 2);
-        flag[cur * 2] = true;
-      }
-      q.pop();
+    int cur = q.front();
+    if (cur == K) {
+      cout << check[cur] - 1 << '\n';
+      return 0;
     }
-    ++r;
+    if (cur + 1 < 100001 && !check[cur + 1]) {
+      q.push(cur + 1);
+      check[cur + 1] = check[cur] + 1;
+    }
+    if (cur - 1 >= 0 && !check[cur - 1]) {
+      q.push(cur - 1);
+      check[cur - 1] = check[cur] + 1;
+    }
+    if (cur * 2 < 100001 && !check[cur * 2]) {
+      q.push(cur * 2);
+      check[cur * 2] = check[cur] + 1;
+    }
+    q.pop();
   }
-  cout << r << '\n';
 }
