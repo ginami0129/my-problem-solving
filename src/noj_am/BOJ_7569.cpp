d48a046449716ff49059d71d4335d4dacc26e683
ginami0129g <ginami0129n@naver.com>
2021-03-15 21:36:39 +0900

    Update BOJ_7569.cpp

--- src/noj_am/BOJ_7569.cpp
+++ src/noj_am/BOJ_7569.cpp
@@ -2,12 +2,10 @@
 #include <queue>
 using namespace std;
 
-int M, N, H;
-int v[101][101][101];
-int dx[] = {1, -1, 0, 0, 0, 0};
-int dy[] = {0, 0, 1, -1, 0, 0};
-int dz[] = {0, 0, 0, 0, 1, -1};
 queue<pair<int, pair<int, int> > > q;
+int v[101][101][101];
+int d[10] = {1, -1, 0, 0, 0, 0, 1, -1, 0, 0};
+int M, N, H;
 
 int main(void) {
   cin.tie(NULL);
@@ -25,17 +23,16 @@ int main(void) {
     }
   }
   while (!q.empty()) {
-    int x = q.front().first;
-    int y = q.front().second.first, z = q.front().second.second;
-    q.pop();
+    int x = q.front().first, y = q.front().second.first, z = q.front().second.second;
     for (int i = 0; i < 6; ++i) {
-      int nx = x + dx[i], ny = y + dy[i], nz = z + dz[i];
-      if (nx < 0 || nx >= H || ny < 0 || ny >= N || nz < 0 || nz >= M) continue;
+      int nx = x + d[i], ny = y + d[i + 2], nz = z + d[i + 4];
+      if (nx < 0 || ny < 0 || nz < 0 || nx >= H || ny >= N || nz >= M) continue;
       if (v[nx][ny][nz] == 0) {
         q.push(make_pair(nx, make_pair(ny, nz)));
         v[nx][ny][nz] = v[x][y][z] + 1;
       }
     }
+    q.pop();
   }
   int r = 0;
   for (int i = 0; i < H; ++i) {
