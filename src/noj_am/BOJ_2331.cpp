33243a97398a75ae1ed2b1ac5e21483b08d9885b
ginami0129g <ginami0129n@naver.com>
2021-03-24 00:35:37 +0900

    Update BOJ_2331.cpp

--- src/noj_am/BOJ_2331.cpp
+++ src/noj_am/BOJ_2331.cpp
@@ -1,23 +1,30 @@
 #include <cmath>
 #include <iostream>
-#include <unordered_map>
+#include <vector>
 using namespace std;
 
-unordered_map<int, int> check;
-int A, P, temp;
+vector<int> check(1000000, 0);
+int A, P, ans;
+
+int dfs(int A, int P) {
+  check[A] = ++ans;
+  int next = 0;
+  while (A) {
+    next += pow(A % 10, P);
+    A /= 10;
+  }
+  if (check[next] == 0) {
+    dfs(next, P);
+  } else {
+    ans = check[next];
+  }
+  return ans;
+}
+
 int main(void) {
   cin.tie(NULL);
   ios_base::sync_with_stdio(false);
 
   cin >> A >> P;
-  for (int i = 1; !check[A]; ++i) {
-    check[A] = i;
-    temp = 0;
-    while (A) {
-      temp += pow(A % 10, P);
-      A /= 10;
-    }
-    A = temp;
-  }
-  cout << check[A] - 1 << '\n';
+  cout << dfs(A, P) - 1 << '\n';
 }
