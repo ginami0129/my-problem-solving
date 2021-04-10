d60f54504fe61f7141807c8582b7b7a8b1908fa2
ginami0129g <ginami0129n@naver.com>
2021-04-10 23:59:41 +0900

    Create BOJ_5337.cpp

---
+++ src/noj_am/BOJ_5337.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  cout << ".  .   ." << '\n';
+  cout << "|  | _ | _. _ ._ _  _" << '\n';
+  cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+}
