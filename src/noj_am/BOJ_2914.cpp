cd9748d8d6a47582a33c8704d7bc4e79bd0631c5
ginami0129g <ginami0129n@naver.com>
2021-04-11 23:54:22 +0900

    Create BOJ_2914.cpp

---
+++ src/noj_am/BOJ_2914.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int A, I;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  cin >> A >> I;
+  cout << A * (I - 1) + 1 << '\n';
+}
