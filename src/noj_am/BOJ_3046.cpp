0042c71713e6f93f736a32a36bb1350a34352580
ginami0129g <ginami0129n@naver.com>
2021-04-12 23:37:39 +0900

    Create BOJ_3046.cpp

---
+++ src/noj_am/BOJ_3046.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int R1, S;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  cin >> R1 >> S;
+  cout << 2 * S - R1 << '\n';
+}
