cdf2010fba2c8cb5563a4b02c7c62a03ab7ab1d0
ginami0129g <ginami0129n@naver.com>
2021-04-13 23:47:34 +0900

    Create BOJ_18018.cpp

---
+++ src/noj_am/BOJ_18018.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int y, diff = 2541 - 1998;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  cin >> y;
+  cout << y - diff << '\n';
+}
