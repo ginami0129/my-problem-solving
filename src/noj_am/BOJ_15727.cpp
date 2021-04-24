bb4ccd9b711a27c64fce1779f8298ff66cb86269
ginami0129g <ginami0129n@naver.com>
2021-04-24 23:35:59 +0900

    Create BOJ_15727.cpp

---
+++ src/noj_am/BOJ_15727.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int L;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  cin >> L;
+  cout << (L + 4) / 5 << '\n';
+}
