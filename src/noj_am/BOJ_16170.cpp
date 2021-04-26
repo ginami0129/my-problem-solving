96ff3523ddf49092dccb0c9431cc35178e7f7086
ginami0129g <ginami0129n@naver.com>
2021-04-26 23:56:08 +0900

    Create BOJ_16170.cpp

---
+++ src/noj_am/BOJ_16170.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cout << "2021" << '\n'
+       << "04" << '\n'
+       << "26" << '\n';
+}
