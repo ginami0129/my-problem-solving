e0f963474d7c21915e239f01ae7c8d4875735743
ginami0129g <ginami0129n@naver.com>
2021-05-08 22:19:11 +0900

    Update BOJ_10816.cpp
    lower bound, upper bound

--- src/noj_am/BOJ_10816.cpp
+++ src/noj_am/BOJ_10816.cpp
@@ -2,8 +2,33 @@
 #define MAX 10000000
 using namespace std;
 
-int n, num;
-int cnt[2 * MAX + 1];
+int n, m, target, arr[MAX];
+
+int lowerBound(int low, int high, int target) {
+  int mid;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (arr[mid] >= target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return low;
+}
+
+int upperBound(int low, int high, int target) {
+  int mid;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (target < arr[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return low;
+}
 
 int main(void) {
   cin.tie(NULL);
@@ -11,12 +36,12 @@ int main(void) {
 
   cin >> n;
   for (int i = 0; i < n; ++i) {
-    cin >> num;
-    ++cnt[num + MAX];
+    cin >> arr[i];
   }
-  cin >> n;
-  for (int i = 0; i < n; ++i) {
-    cin >> num;
-    cout << cnt[num + MAX] << ' ';
+  sort(arr, arr + n);
+  cin >> m;
+  for (int i = 0; i < m; ++i) {
+    cin >> target;
+    cout << upperBound(0, n - 1, target) - lowerBound(0, n - 1, target) << ' ';
   }
 }
