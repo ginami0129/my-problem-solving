094de3c6d88734625ab88adef11edff7bbb5f3fb
ginami0129g <ginami0129n@naver.com>
2021-05-11 21:51:04 +0900

    Update BOJ_9461.cpp
    d[n] = d[n-1] + n[n-5] => d[n] = d[n-2] + d[n-3]

--- src/noj_am/BOJ_9461.cpp
+++ src/noj_am/BOJ_9461.cpp
@@ -1,18 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #define MAX 101
 using namespace std;
 
+int n, t;
+long long d[MAX] = {0, 1, 1};
+
 int main(void) {
-  int T, N;
-  long long P[MAX] = {0, 1, 1, 1, 2};
-  ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  for (int i = 5; i <= MAX - 1; ++i) {
-    P[i] = P[i - 1] + P[i - 5];
+  ios_base::sync_with_stdio(false);
+
+  for (int i = 3; i < MAX; ++i) {
+    d[i] = d[i - 2] + d[i - 3];
   }
-  cin >> T;
-  while (T--) {
-    cin >> N;
-    cout << P[N] << '\n';
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    cout << d[n] << '\n';
   }
 }
