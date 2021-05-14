fbb01d50c48b3175301d0e222a0ee485810e9ca3
ginami0129g <ginami0129n@naver.com>
2021-05-14 19:55:54 +0900

    Update BOJ_2225.cpp
    중복조합으로 문제를 해석함

--- src/noj_am/BOJ_2225.cpp
+++ src/noj_am/BOJ_2225.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
-#define MAX 201
-#define OUTPUT 1000000000
+#include <bits/stdc++.h>
+#define MAX 401
+#define M 1000000000
 using namespace std;
 
+int n, k, d[MAX][MAX] = {1};
+
 int main(void) {
-  int N, K;
-  int d[MAX][MAX] = {0};
-  ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  cin >> N >> K;
-  for (int i = 1; i <= K; ++i) {
+  ios_base::sync_with_stdio(false);
+
+  cin >> n >> k;
+  for (int i = 1; i <= n + k - 1; ++i) {
     d[i][0] = 1;
-    for (int j = 1; j <= N; ++j) {
-      d[i][j] = (d[i - 1][j] + d[i][j - 1]) % OUTPUT;
+    for (int j = 1; j <= n; ++j) {
+      d[i][j] = (d[i - 1][j] + d[i - 1][j - 1]) % M;
     }
   }
-  cout << d[K][N] << '\n';
+  cout << d[n + k - 1][n] << '\n';
 }
