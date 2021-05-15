996549e0bdc2b775c16a390b030806c15d8915c0
ginami0129g <ginami0129n@naver.com>
2021-05-15 22:40:31 +0900

    Create BOJ_20492.cpp
    cout에서 기본 출력 자리수는 6 =  cout.precision(6);

---
+++ src/noj_am/BOJ_20492.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  cin >> n;
+  cout.precision(7);
+  cout << n * 0.78 << ' ' << n * 0.956 << '\n';
+}
