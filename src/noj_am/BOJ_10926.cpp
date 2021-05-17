9fd23441e0dd1d10b2ecd640ac4db58ebd10b71b
ginami0129g <ginami0129n@naver.com>
2021-05-17 23:56:36 +0900

    Create BOJ_10926.cpp
    삼중자

---
+++ src/noj_am/BOJ_10926.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string name;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  cin >> name;
+  cout << name << "?\?!" << '\n';
+}
