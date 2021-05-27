d0c8af84e9e08472a0e7b9ad278d9538b66b2122
ginami0129g <ginami0129n@naver.com>
2021-05-27 23:48:40 +0900

    Create BOJ_11382.cpp

---
+++ src/noj_am/BOJ_11382.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b, c;
+
+int main(void) {
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin >> a >> b >> c;
+  cout << a + b + c << '\n';
+}
